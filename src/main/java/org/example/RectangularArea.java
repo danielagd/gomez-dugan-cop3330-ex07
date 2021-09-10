6fae47c2e8d45b47707ecac5459528655409bc04
Daniela <danigomezdugan@gmail.com>
2021-09-09 21:32:57 -0400

    updating file

--- src/main/java/org/example/RectangularArea.java
+++ src/main/java/org/example/RectangularArea.java
@@ -8,12 +8,15 @@ public class RectangularArea
 {
     public static void main( String[] args )
     {
+        // Ask for dimensions of a room and calculate the area in square meters
         final double conversion_factor = 0.09290304;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("What is the length of the room in feet? ");
         double length = input.nextDouble();
+
+        // Checking for negative values
         while (length < 0) {
             System.out.print("Negative values are invalid. Please enter a positive length in feet: ");
             length = input.nextDouble();
@@ -21,6 +24,8 @@ public class RectangularArea
 
         System.out.print("What is the width of the room in feet? ");
         double width = input.nextDouble();
+
+        // Checking for negative values
         while (width < 0) {
             System.out.print("Negative values are invalid. Please enter a positive width in feet: ");
             width = input.nextDouble();
@@ -28,9 +33,11 @@ public class RectangularArea
 
         System.out.println("You entered dimensions of " + length + " feet by " + width + " feet.");
 
+        // Square Feet
         double area = length * width;
         System.out.format("The area is: \n%.2f square feet\n", area);
 
+        // Square Meters
         double meters = area * conversion_factor;
         System.out.format("%.3f square meters", meters);
 
